afe3861f63c285372ed3f7f9529394ce0d6197b3
Firede <firede@firede.us>
2013-10-16 03:17:07 +0800

    init saber-lang.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+/**
+ * saber-lang
+ * 
+ * @file  lang
+ * @author  firede[firede@firede.us]
+ */
+
+define(function( require ) {
+
+    return {
+        inherits: require( './inherits' ),
+        extend: require( './extend' ),
+        curry: require( './curry' )
+    }
+
+});
